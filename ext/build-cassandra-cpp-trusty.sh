54e256acbcaccd900404e82b3c69a050f9efd144
Ludovic LASSELIN <ludovic.lasselin@orange.com>
2016-05-23 10:23:47 +0200

    add packaging for trusty

--- ext/build-cassandra-cpp-trusty.sh
+++ ext/build-cassandra-cpp-trusty.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-git clone http://github.com/datastax/cpp-driver.git
+#git clone http://github.com/datastax/cpp-driver.git
 #git clone https://github.com/waitamer/cpp-driver
-mkdir cpp-driver/build
-cd cpp-driver/build
+#mkdir cpp-driver/build
+#cd cpp-driver/build
 #git checkout feature/blacklist
-builddir=`pwd`
-cmake ..
-make
-make install
-mv libcassandra_static.a libcassandra.a
+#builddir=pwd
+#cmake -DCMAKE_CXX_FLAGS="-fPIC" -DCMAKE_INSTALL_PREFIX:PATH=$builddir -DCASS_BUILD_STATIC=ON \
+#  -DCASS_BUILD_SHARED=OFF -DCMAKE_BUILD_TYPE=RELEASE -DCASS_USE_ZLIB=ON \
+#  -DCMAKE_INSTALL_LIBDIR:PATH=lib ..
+#make
+#make install
+#mv libcassandra_static.a libcassandra.a
+
+git submodule update --init
+cd ext
+./install.sh
